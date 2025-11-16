4e0f75241427fb386f509411db32012d0ce2db95
Adrian <teebee15@hotmail.com>
2025-11-16 00:36:48 -0500

    oño

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,11 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  // Debe coincidir con lo que pusimos en authOptions
+  pages: {
+    signIn: "/signin",
+  },
+});
 
 export const config = {
   matcher: ["/dashboard", "/profile"],
